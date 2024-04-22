179129c18cb3b07275e4693a7dd8827d7694bb95
Annlyn <annlynanto@gmail.com>
2024-04-22 10:30:08 +0530

    cards created with dummy data

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import { FaRegFileAlt } from "react-icons/fa";
+import { LuDownload } from "react-icons/lu";
+import { IoClose } from "react-icons/io5";
 
-function Card() {
-  return <div className="w-60 h-72 rounded-[20px] bg-zinc-700/70"></div>;
+function Card({ data }) {
+  return (
+    <div className="relative flex-shrink-0 w-60 h-72 rounded-[40px] bg-zinc-800 text-white py-10 px-8 overflow-hidden">
+      <FaRegFileAlt />
+      <p className="text-sm leading-tight mt-5 font-semibold">{data.desc}</p>
+      <div className="footer absolute bottom-0  w-full left-0 ">
+        <div className="flex items-center justify-between mb-3 py-3 px-8">
+          <h5>{data.fileSize}</h5>
+          <span className="w-7 h-7 bg-zinc-600 rounded-full flex items-center justify-center text-black cursor-pointer">
+            {data.close ? <IoClose size=".75em" /> : <LuDownload size=".75em" color="#fff" />}
+          </span>
+        </div>
+        {data.tag.isOpen ? (
+          <div
+            className={`tag w-full bg-${data.tag.tagColor}-700 py-4 flex items-center justify-center`}
+          >
+            <h3 className="text-sm font-semibold">{data.tag.tagTitle}</h3>
+          </div>
+        ) : null}
+      </div>
+    </div>
+  );
 }
 
 export default Card;
--- src/components/Foreground.jsx
+++ src/components/Foreground.jsx
@@ -2,9 +2,32 @@ import React from "react";
 import Card from "./Card";
 
 function Foreground() {
+  const dummyData = [
+    {
+      desc: "Lorem ipsum dolor sit amet.",
+      fileSize: ".9mb",
+      close: false,
+      tag: { isOpen: true, tagTitle: "Download Now", tagColor: "sky" },
+    },
+    {
+      desc: "Lorem ipsum dolor sit amet consectetur",
+      fileSize: ".9mb",
+      close: false,
+      tag: { isOpen: false, tagTitle: "Download Now", tagColor: "sky" },
+    },
+    {
+      desc: "Lorem ipsum dolor, sit amet consectetur adipisicing",
+      fileSize: ".2mb",
+      close: true,
+      tag: { isOpen: true, tagTitle: "Download Now", tagColor: "green" },
+    },
+  ];
+
   return (
-    <div className="w-full h-full fixed z-[3] top-0 left-0">
-      <Card />
+    <div className="w-full h-full fixed z-[3] top-0 left-0 flex gap-10 flex-wrap p-5">
+      {dummyData.map((data, index) => (
+        <Card data={data} key={index} />
+      ))}
     </div>
   );
 }
