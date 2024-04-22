87cb632ad1fd7c6c7f9c6ef1417b03d0e84ba17a
Annlyn <annlynanto@gmail.com>
2024-04-22 17:37:08 +0530

    framer motion added for dragging cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,10 +2,21 @@ import React from "react";
 import { FaRegFileAlt } from "react-icons/fa";
 import { LuDownload } from "react-icons/lu";
 import { IoClose } from "react-icons/io5";
+import { motion } from "framer-motion";
 
-function Card({ data }) {
+function Card({ data, reference }) {
   return (
-    <div className="relative flex-shrink-0 w-60 h-72 rounded-[40px] bg-zinc-800 text-white py-10 px-8 overflow-hidden">
+    <motion.div
+      drag
+      dragConstraints={reference}
+      whileDrag={{ scale: 1.2 }}
+      dragElastic={0.2}
+      dragTransition={{
+        bounceStiffness: 100,
+        bounceDamping: 30,
+      }}
+      className="relative flex-shrink-0 w-60 h-72 rounded-[40px] bg-zinc-800 text-white py-10 px-8 overflow-hidden"
+    >
       <FaRegFileAlt />
       <p className="text-sm leading-tight mt-5 font-semibold">{data.desc}</p>
       <div className="footer absolute bottom-0  w-full left-0 ">
@@ -17,13 +28,15 @@ function Card({ data }) {
         </div>
         {data.tag.isOpen ? (
           <div
-            className={`tag w-full bg-${data.tag.tagColor}-700 py-4 flex items-center justify-center`}
+            className={`tag w-full ${
+              "bg-" + data.tag.tagColor + "-700"
+            } py-4 flex items-center justify-center`}
           >
             <h3 className="text-sm font-semibold">{data.tag.tagTitle}</h3>
           </div>
         ) : null}
       </div>
-    </div>
+    </motion.div>
   );
 }
 
--- src/components/Foreground.jsx
+++ src/components/Foreground.jsx
@@ -1,32 +1,50 @@
-import React from "react";
+import React, { useRef } from "react";
 import Card from "./Card";
+import { motion } from "framer-motion";
 
 function Foreground() {
   const dummyData = [
     {
       desc: "Lorem ipsum dolor sit amet.",
       fileSize: ".9mb",
-      close: false,
-      tag: { isOpen: true, tagTitle: "Download Now", tagColor: "sky" },
+      close: true,
+      tag: { isOpen: true, tagTitle: "Upload", tagColor: "blue" },
     },
     {
       desc: "Lorem ipsum dolor sit amet consectetur",
       fileSize: ".9mb",
       close: false,
-      tag: { isOpen: false, tagTitle: "Download Now", tagColor: "sky" },
+      tag: { isOpen: true, tagTitle: "Download Now", tagColor: "green" },
+    },
+    {
+      desc: "Lorem ipsum dolor, sit amet consectetur adipisicing",
+      fileSize: ".2mb",
+      close: false,
+      tag: { isOpen: true, tagTitle: "Download Now", tagColor: "green" },
+    },
+    {
+      desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit",
+      fileSize: ".26mb",
+      close: false,
+      tag: { isOpen: false, tagTitle: "Download Now", tagColor: "green" },
     },
     {
       desc: "Lorem ipsum dolor, sit amet consectetur adipisicing",
       fileSize: ".2mb",
       close: true,
-      tag: { isOpen: true, tagTitle: "Download Now", tagColor: "green" },
+      tag: { isOpen: true, tagTitle: "Upload", tagColor: "blue" },
     },
   ];
 
+  const ref = useRef(null);
+
   return (
-    <div className="w-full h-full fixed z-[3] top-0 left-0 flex gap-10 flex-wrap p-5">
+    <div
+      ref={ref}
+      className="w-full h-full fixed z-[3] top-0 left-0 flex gap-10 flex-wrap p-5"
+    >
       {dummyData.map((data, index) => (
-        <Card data={data} key={index} />
+        <Card data={data} key={index} reference={ref} />
       ))}
     </div>
   );
